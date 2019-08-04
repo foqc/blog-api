9f26e0e0d5a01c68ca85ce9b7637b2c7d9401725
foqc <fabiaq44@gmail.com>
2019-08-04 11:16:07 -0500

    adding file date-name-extension format when file is uploading

--- api/controllers/FileController.js
+++ api/controllers/FileController.js
@@ -1,4 +1,5 @@
 exports.create_a_file = function (req, res) {
+    console.log(':::::::::::::::::::::::::::::::::::::::::::::::::<<<<<<<<<<<<<<<<<<<<<<< ', req.file)
     if (!req.file) res.status(500).json({ errors: { global: 'Please upload a file' } });
     else
         res.json(req.file.path);
